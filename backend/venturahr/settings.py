8e941eec0c27e5e2142f8e4fa8001733070e99ad
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2022-06-18 15:34:40 -0300

    feat: add opportunities app

--- backend/venturahr/settings.py
+++ backend/venturahr/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # externals,
+    # externals
     "rest_framework",
+    # custom
+    "opportunities",
 ]
 
 MIDDLEWARE = [
