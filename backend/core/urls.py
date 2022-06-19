6a3b9a0154d6ac5fc53a7dbc460ffaf9589e1e47
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2022-06-19 13:37:25 -0300

    (feat)core: add registration page

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -8,4 +8,5 @@ app_name = "core"
 urlpatterns = [
     path("home/", views.HomePageView.as_view(), name="home"),
     path("login/", views.VenturaHRLoginView.as_view(), name="login"),
+    path("register/", views.VenturaHRRegisterView.as_view(), name="register"),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,18 +1,29 @@
 import logging
+from abc import ABC
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from django.views import View, generic
+
+from core.forms import SignUpForm
 
 logger = logging.getLogger(__name__)
 
 
-class VenturaHRView(View):
+class VenturaHRView(ABC, View):
     ...
 
 
 class HomePageView(VenturaHRView, generic.base.TemplateView):
     template_name = "core/home.html"
+
+
+class VenturaHRRegisterView(generic.CreateView):
+    form_class = SignUpForm
+
+    template_name = "core/register.html"
+    success_url = reverse_lazy("core:login")
 
 
 class VenturaHRLoginView(LoginView):
