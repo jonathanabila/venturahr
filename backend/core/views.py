f43d0e6ac795036979c8b76d2654ca1ac3e9df4f
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2022-08-20 23:31:07 -0300

    feat: redirect for authenticated users

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,13 +2,23 @@ import logging
 from abc import ABC
 
 from django.contrib.auth.views import LogoutView
-from django.views import View, generic
+from django.http.response import HttpResponseRedirectBase
+from django.shortcuts import redirect
+from django.views import generic
+from django.views.generic import TemplateView
 
 logger = logging.getLogger(__name__)
 
 
-class VenturaHRView(ABC, View):
-    ...
+class VenturaHRView(ABC, TemplateView):
+    def dispatch(self, request, *args, **kwargs) -> HttpResponseRedirectBase:
+        # If private is the path, and we redirect, we'll have a nice loop
+        if (user := request.user) and user.is_authenticated and "private" not in request.path:
+            if user.company:
+                return redirect("companies:private-home")
+            else:
+                return redirect("candidates:private-home")
+        return super().dispatch(request, *args, **kwargs)
 
 
 class HomePageView(VenturaHRView, generic.base.TemplateView):
