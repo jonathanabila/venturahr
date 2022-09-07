1c0e6eda34fc3d06de2b87ca69e8665d879c66bd
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2022-09-07 13:07:05 -0300

    feat: add opportunity requirement model

--- backend/opportunities/models.py
+++ backend/opportunities/models.py
@@ -4,10 +4,41 @@ from django.utils import timezone
 
 class Opportunity(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=254)
+    description = models.TextField(max_length=3000)
 
     created_at = models.DateTimeField(default=timezone.now)
     expires_at = models.DateField()
+
+    company = models.ForeignKey(
+        "companies.Company",
+        null=True,
+        on_delete=models.CASCADE,
+        verbose_name="company",
+        help_text="User company",
+    )
+
+    created_by = models.ForeignKey(
+        "core.User",
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name="created by",
+    )
+
+
+class OpportunityRequirement(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField(max_length=3000)
+
+    created_at = models.DateTimeField(default=timezone.now)
+
+    weight = models.IntegerField(null=False)
+
+    opportunity = models.ForeignKey(
+        "opportunities.Opportunity",
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name="opportunity",
+    )
 
     company = models.ForeignKey(
         "companies.Company",
