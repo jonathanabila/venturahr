d6a7bace72f577acdf86fafb3313c93bd097a48f
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2022-09-13 23:18:39 -0300

    feat: add opportunity answer / requirements

--- backend/opportunities/models.py
+++ backend/opportunities/models.py
@@ -61,3 +61,54 @@ class OpportunityRequirement(models.Model):
         on_delete=models.CASCADE,
         verbose_name="created by",
     )
+
+
+class OpportunityAnswer(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+
+    opportunity = models.ForeignKey(
+        "opportunities.Opportunity",
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name="opportunity",
+        related_name="answers",
+    )
+
+    created_by = models.ForeignKey(
+        "core.User",
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name="created by",
+    )
+
+
+class OpportunityAnswerRequirement(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    description = models.TextField(max_length=3000)
+
+    answer = models.IntegerField(
+        null=False, validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+
+    opportunity_answer = models.ForeignKey(
+        "opportunities.OpportunityAnswer",
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name="answer",
+        related_name="requirements",
+    )
+
+    opportunity_requirement = models.ForeignKey(
+        "opportunities.OpportunityRequirement",
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name="answer",
+        related_name="requirements",
+    )
+
+    created_by = models.ForeignKey(
+        "core.User",
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name="created by",
+    )
