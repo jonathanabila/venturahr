e5778e2616c451a7edade2c89e13e0122fffd7f5
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2022-09-20 21:38:33 -0300

    test: check candidate permissions

--- backend/candidates/test_forms.py
+++ backend/candidates/test_forms.py
@@ -20,3 +20,39 @@ class TestCandidatesRegistrationForm:
 
         form = self.sut(factory_boy_decoder(payload))
         assert form.is_valid() is False
+
+    def test_form_is_valid(self) -> None:
+        payload = factory_boy_decoder(UserFormFactory())
+
+        form = self.sut(factory_boy_decoder(payload))
+        assert form.is_valid() is True
+
+    def test_candidate_permissions(self) -> None:
+        payload = factory_boy_decoder(UserFormFactory())
+
+        form = self.sut(factory_boy_decoder(payload))
+        form.is_valid()
+
+        user = form.save()
+
+        user.has_perms(
+            tuple(
+                f"opportunities.{p}"
+                for p in (
+                    "view_candidateuser",
+                    "change_candidateuser",
+                    "view_opportunity",
+                    "signup_opportunity",
+                )
+            )
+        )
+
+    def test_candidate_form_permissions_is_candidate(self):
+        payload = factory_boy_decoder(UserFormFactory())
+
+        form = self.sut(factory_boy_decoder(payload))
+        form.is_valid()
+
+        user = form.save()
+
+        assert user.is_candidate is True
