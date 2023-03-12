16fc6f9e657ccc8e7aaafae8a30b15a6b5d825d3
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2023-03-12 14:58:19 -0300

    lint: update migrations lint errors

--- backend/opportunities/migrations/0006_opportunityanswer_opportunityanswerrequirement.py
+++ backend/opportunities/migrations/0006_opportunityanswer_opportunityanswerrequirement.py
@@ -8,7 +8,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("opportunities", "0005_alter_opportunity_options"),
