d42aa3286070b7c02c5b340951c01b2670dfa0bd
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2023-03-26 14:43:56 -0300

    lint: fix a bunch of code smells

--- backend/venturahr/views.py
+++ backend/venturahr/views.py
@@ -3,6 +3,6 @@ import os
 from django.http import HttpResponse
 
 
-def index(request):
+def index(*_, **__):
     version = os.getenv("COMMIT_HASH")
     return HttpResponse(f"VenturaHR@{version}")
